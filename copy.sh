cf92799eefd89ff739f1ba1a6d37183dc9f287a5
Fahmidha-pa <116252309+Fahmidha-pa@users.noreply.github.com>
2023-06-19 14:14:26 +0530

    Create copy.sh

---
+++ copy.sh
@@ -0,0 +1,21 @@
+#copy set of files as pairs
+r=`expr $# % 2`
+if [ $r -ne 0 ]
+then
+echo "file name are not pairs "
+exit 1
+fi
+while [ $# -ne 0 ]
+do
+cp $1 $2
+shift
+shift
+done
+exit 0 
+
+
+#output
+
+ksb@ksb-H410M-S2-V2:~/shell$ sh cp.sh abc abcd
+ksb@ksb-H410M-S2-V2:~/shell$ sh cp.sh abc abcd a
+file name are not pairs 
